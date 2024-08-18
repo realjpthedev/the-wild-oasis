e5f28054b38cbd38dbd5f3079fab2ef4b9fcc816
jpucket8 <jp@jpthedev.com>
2024-08-18 16:43:40 -0400

    implement authentication

---
+++ src/services/apiAuth.js
@@ -0,0 +1,17 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+	let { data, error } = await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
+
+	if (error) {
+		console.error(error);
+		throw new Error(error.message);
+	}
+
+	console.log(data);
+
+	return data;
+}
