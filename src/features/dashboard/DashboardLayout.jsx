17f2a742b501c5ddd5622df8df0e6c643ab7bde0
jpucket8 <jp@jpthedev.com>
2024-08-19 17:09:33 -0400

    implement data fetching for dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,29 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	const { bookings, isLoading1 } = useRecentBookings();
+	const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+	if (isLoading1 || isLoading2) return <Spinner />;
+
+	console.log(bookings);
+	return (
+		<StyledDashboardLayout>
+			<div>Statistics</div>
+			<div>Today's activity</div>
+			<div>Chart stay durations</div>
+			<div>Chart sales</div>
+		</StyledDashboardLayout>
+	);
+}
+export default DashboardLayout;
