96f775b1c5b927de06baf8f1541b44be913960ce
jpucket8 <jp@jpthedev.com>
2024-08-19 15:47:00 -0400

    implement user account page and logic

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -48,3 +48,38 @@ export async function logout() {
 
 	if (error) throw new Error(error.message);
 }
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+	// 1. Update password OR fullName
+	let updateData;
+
+	if (password) updateData = { password };
+
+	if (fullName) updateData = { data: { fullName } };
+
+	const { data, error } = await supabase.auth.updateUser(updateData);
+
+	if (error) throw new Error(error.message);
+
+	if (!avatar) return data;
+
+	// 2. Upload avatar image
+	const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+	const { error: storageError } = await supabase.storage
+		.from("avatars")
+		.upload(fileName, avatar);
+
+	if (storageError) throw new Error(storageError.message);
+
+	// 3. Update avatar in the user
+	const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+		data: {
+			avatar: `https://vktkbhhbuhseycplpjss.supabase.co/storage/v1/object/public/avatars/${fileName}`,
+		},
+	});
+
+	if (error2) throw new Error(error.message);
+
+	return updatedUser;
+}
