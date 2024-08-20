41aa8ec9938c5f19dfd565ad38ad554d9a9a25cb
jpucket8 <jp@jpthedev.com>
2024-08-20 15:20:14 -0400

    bug fixes

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -8,6 +8,7 @@ import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 import Spinner from "../../ui/Spinner";
+import Empty from "../../ui/Empty";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
@@ -32,6 +33,7 @@ function BookingDetail() {
 	const moveBack = useMoveBack();
 
 	if (isLoading) return <Spinner />;
+	if (!booking) return <Empty resourceName="booking" />;
 
 	const { status, id: bookingId } = booking;
 
